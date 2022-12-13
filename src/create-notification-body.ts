a7f79bbf16630d4fd1fca0a71157c72ad9cf6b32
César Hilário <cesar.hilario.dev@gmail.com>
2022-12-13 20:39:37 -0300

    feat: [GET][POST] notifications

---
+++ src/create-notification-body.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, IsUUID, Length } from 'class-validator';
+
+export class CreateNotificationBody {
+  @IsNotEmpty()
+  @IsUUID()
+  public recipientId: string;
+
+  @IsNotEmpty()
+  @Length(5, 240)
+  public content: string;
+
+  @IsNotEmpty()
+  public category: string;
+}
