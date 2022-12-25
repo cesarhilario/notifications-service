3656e49c5fb801bba39dd4bf2dcb4e023680aa6a
César Hilário <cesar.hilario.dev@gmail.com>
2022-12-25 20:29:30 -0300

    chore: add producer-notification.send-notificaiton.mjs script

---
+++ scritpts/producer-notifications.send-notification.mjs
@@ -0,0 +1,39 @@
+import { Kafka } from 'kafkajs';
+import { randomUUID } from 'node:crypto';
+
+import env from 'dotenv';
+env.config();
+
+async function bootstrap() {
+  console.log(process.env);
+  const kafka = new Kafka({
+    clientId: 'test-producer',
+    brokers: [process.env.KAFKA_BROKER ?? ''],
+    sasl: {
+      mechanism: 'scram-sha-256',
+      username: process.env.KAFKA_SASL_USERNAME ?? '',
+      password: process.env.KAFKA_SASL_PASSWORD ?? '',
+    },
+    ssl: true,
+  });
+
+  const producer = kafka.producer();
+
+  await producer.connect();
+  await producer.send({
+    topic: 'notifications.send-notification',
+    messages: [
+      {
+        value: JSON.stringify({
+          content: 'Nova solicitação de amizade via kafka',
+          category: 'social',
+          recipientId: randomUUID(),
+        }),
+      },
+    ],
+  });
+
+  await producer.disconnect();
+}
+
+bootstrap();
